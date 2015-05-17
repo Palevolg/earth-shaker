28e950ec00b425dc981e4127d17a5fdd8f4a6501
LDO_Den <ldo@tut.by>
2015-05-17 18:37:49 +0200

    moving and ground eating

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public GameObject player;
-
+	private Vector2 pos;
 	public void placeToXY (int x, int y) {
 
 	}
@@ -16,6 +16,47 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		pos = transform.position;
+		
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			//RaycastHit2D hit;
+			//Vector2 start = pos;
+			//Vector2 end = start + new Vector2(pos.x + 1, pos.y);
+			//hit = Physics2D.Linecast(start, end);
+			//Debug.Log(hit.collider);
+			//if (hit.transform == null){ 
+				pos.x += 1;
+				transform.position = pos;
+			//}
+			
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			pos.x -= 1;
+			transform.position = pos; 		
+			
+		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			pos.y += 1;
+			transform.position = pos; 		
+			
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			pos.y -= 1;
+			transform.position = pos; 		
+			
+		}
+		
+
+	}
+	void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		// Is this a shot?
+		GroundScript ground = otherCollider.gameObject.GetComponent<GroundScript>();
+		if (ground != null)
+		{
+			// Destroy the ground
+			Destroy(ground.gameObject); 
+			
+		}
 	}
 }
