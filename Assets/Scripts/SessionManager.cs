9bdb5ba0cd195a8f00088b42acd81e212b530a10
Oleg Palevich <palevolg@gmail.com>
2015-05-26 08:01:30 +0300

    SessionManager Singleton

---
+++ Assets/Scripts/SessionManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SessionManager {
+
+	private static SessionManager instance;
+
+	public static SessionManager GetInstance() {
+		if (instance == null)
+			instance = new SessionManager ();
+		return instance;
+	}
+}
