d1f1ddd9c3946e77bbb6afe0a6503318e8261cbf
Oleg Palevich <palevolg@gmail.com>
2015-05-28 20:19:11 +0300

    Profile singleton created

---
+++ Assets/Scripts/ProfileManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProfileManager {
+	
+	string name;
+	
+	private static ProfileManager instance;
+	
+	public static ProfileManager GetInstance() {
+		if (instance == null)
+			instance = new ProfileManager ();
+		return instance;
+	}
+
+	public void SetName(string n) {
+		name = n;
+	}
+
+	public string GetName() {
+		return name;
+	}
+
+	public void Load() {
+		//todo load profile
+	}
+	
+	public void Save() {
+		//todo save provile
+	}
+
+	public void Create() {
+		//create profile
+	}
+}
